7b35c060a942b1e4ac1799d8691bdb14eb986692
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-21 12:59:09 -0500

    completed function 3

--- functions_basic.py
+++ functions_basic.py
@@ -16,3 +16,13 @@ print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_
 output prediction: error number_of_days_in_a_week_silicon_or_triangle_sides() undefined
 correct output: error number_of_days_in_a_week_silicon_or_triangle_sides() undefined
 """
+
+#3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+"""
+output prediction: 5
+correct output: 5
+"""
