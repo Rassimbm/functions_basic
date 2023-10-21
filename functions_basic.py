73aaa75fd8f29a5029ecfc74bc03008e3ed8145a
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-21 12:58:01 -0500

    completed function 2

--- functions_basic.py
+++ functions_basic.py
@@ -6,3 +6,13 @@ print(number_of_food_groups())
 output prediction: 5
 correct output: 5
 """
+
+# 2
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+"""
+output prediction: error number_of_days_in_a_week_silicon_or_triangle_sides() undefined
+correct output: error number_of_days_in_a_week_silicon_or_triangle_sides() undefined
+"""
